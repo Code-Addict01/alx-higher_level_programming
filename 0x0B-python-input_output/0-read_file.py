46aca3c1323b41f5d758df47c922f6150b283f19
Code-Addict01 <davidckihoto@gmail.com>
2024-08-09 00:16:43 +0300

    Task 1 of python input, output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function that reads a text-file"""
+
+def read_file(filename=""):
+    """Print content of UTF8 encoded file to the stdout"""
+
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
