ded8bd4e09126ab2b9f9e66ea6c6d5b08bc371c8
Code-Addict01 <davidckihoto@gmail.com>
2024-08-14 13:27:27 +0300

    semistandard errors removed

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,11 +3,11 @@ const dict = require('./101-data').dict;
 const dKeys = Object.keys(dict);
 const values = Object.values(dict);
 let matched;
-const res;
+const res = {};
 
 for (let i = 0; i < values.length; i++) {
-  result[JSON.stringify(values[i]) = [];
+  res[JSON.stringify(values[i])] = [];
   matched = dKeys.filter(key => dict[key] === values[i]);
-  matched.forEach(item => result[JSON.stringify(value[i])].push(item));
+  matched.forEach(item => res[JSON.stringify(values[i])].push(item));
 }
-console.log(result);
+console.log(res);
