95aa39b3c85b8c49aa93cbc870961fba05d61e46
Code-Addict01 <davidckihoto@gmail.com>
2024-08-14 13:46:46 +0300

    Task 9 of JavaScript objects, scopes, and closures

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   let len = list.length - 1;
-  for (let i = 0; (len - 1) > 0; i++) {
+  for (let i = 0; (len - i) > 0; i++) {
     const tmp = list[len];
     list[len] = list[i];
     list[i] = tmp;
